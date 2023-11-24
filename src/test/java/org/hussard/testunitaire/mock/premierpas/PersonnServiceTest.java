089d920976e49cdd076455eb3a8852f601ce239d
stephane betton <stephane.betton@ag2rlamondiale.fr>
2023-11-24 13:25:10 +0100

    docs: doc  Custom Postfix Templates

--- src/test/java/org/hussard/testunitaire/mock/premierpas/PersonnServiceTest.java
+++ src/test/java/org/hussard/testunitaire/mock/premierpas/PersonnServiceTest.java
@@ -3,6 +3,7 @@ package org.hussard.testunitaire.mock.premierpas;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 class PersonnServiceTest {
     @Test
@@ -14,5 +15,11 @@ class PersonnServiceTest {
         // on vérifie que le résultat est bien celui attendu
         assertThat(resultat.getNom()).isEqualTo("Doe");
         assertThat(resultat.getPrenom()).isEqualTo("John");
+        Throwable thrown = catchThrowable(() -> resultat.getPrenom());
+        assertThat(thrown)
+                .isInstanceOf(Exception.class)
+                .hasMessage("");
+
+
     }
 }
